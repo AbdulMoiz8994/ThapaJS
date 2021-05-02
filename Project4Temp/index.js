498773bd2d53425a526e11c39656d5c93a6ecc17
AbdulMoiz8994 <moiza8994@gmail.com>
2021-05-02 08:55:19 +0500

    we made termemeter and we used  innerHTML whihc update replace and  add icons value in our tag, settimout which we call automatcally  specfic time OUt() we only give functon/variable as per req we also add css var.style.color="", use SetInterval( for infinte loop aftr evry 5 second it will automticall and then gain u=our whole function tempload runs

---
+++ Project4Temp/index.js
@@ -0,0 +1,38 @@
+
+// via templaod function we can call at a time to fucntion
+// .innerHTML we can pdate replace or put the value icon etch in our tag via first etting id
+const tempload=() =>{
+  const temp=document.getElementById("temp")  
+  temp.innerHTML='&#xf2cb;'
+
+  window.setTimeout(() => {
+      temp.innerHTML='&#xf2ca;'
+      temp.style.color="#32CD32"
+
+  },1000)
+
+  window.setTimeout(() => {
+    temp.innerHTML='&#xf2c9;'
+    // we can add the css in Js
+    temp.style.color="#32CD32"
+},2000)
+
+window.setTimeout(() => {
+    temp.innerHTML='&#xf2c8;';
+    temp.style.color="yellow"
+},3000)
+
+window.setTimeout(() => {
+    temp.innerHTML='&#xf2c7;'
+    temp.style.color="#d63031"
+},4000)
+
+
+}
+tempload()
+
+window.setInterval(() =>{
+    tempload()
+    temp.style='white'
+},5000)
+
